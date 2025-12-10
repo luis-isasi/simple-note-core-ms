ea681f930cb5f36b5e6313d197123beeef6a3b82
unknown <luis16isasi@gmail.com>
2025-12-09 20:38:10 -0500

    feat: use uuid

--- src/controller/SimpleNoteController.ts
+++ src/controller/SimpleNoteController.ts
@@ -1,10 +1,13 @@
+import { v5 } from 'uuid';
+
 export class SimpleNoteController {
   constructor() {}
 
   async testMethod() {
+    const uuid = v5('test', 'test');
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Hello, world!' }),
+      body: JSON.stringify({ message: 'Hello, world!', uuid }),
     };
   }
 }
