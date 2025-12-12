a9903e2eb223c310aaea7143ff97fafe950cfa19
unknown <luis16isasi@gmail.com>
2025-12-12 11:02:38 -0500

    feat: change uuid

--- src/controller/SimpleNoteController.ts
+++ src/controller/SimpleNoteController.ts
@@ -4,10 +4,11 @@ export class SimpleNoteController {
   constructor() {}
 
   async testMethod() {
-    const uuid = v5('test', 'test');
+    // const uuid = v5('test', 'test');
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Hello, world!', uuid }),
+      body: JSON.stringify({ message: 'Hello, world!', uuid: 'asfafsnfa' }),
     };
   }
 }
